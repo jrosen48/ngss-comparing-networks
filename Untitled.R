0cb617904dd9d3c8ecc6c290356d238f831cc41d
jrosen48 <jmrosen48@gmail.com>
2023-09-07 09:08:51 -0400

    update

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+library(academictwitteR)
+
+get_all_tweets(start_tweets = "2023-06-20T00:00:00Z", 
+               end_tweets = "2023-06-30T00:00:00Z",
+               bearer_token = "AAAAAAAAAAAAAAAAAAAAAKK%2FQwEAAAAAs%2FSScNSd9ZE6Oczs1UgPWzvNDx4%3DdrV01JxorEhRf3qZCfKsIcqnbPeYGcjiGtbu55TDro7rHqaHJg", 
+               n = 500, 
+               query = "test")
